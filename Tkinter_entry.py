38b8c22d88c3cc9d03a7fa38520a9dbb79123291
Christian Thulstrup <ct.5000@hotmail.com>
2015-01-07 14:09:09 +0100

    More test

--- Tkinter_entry.py
+++ Tkinter_entry.py
@@ -1,1 +1,24 @@
+from Tkinter import *
 
+def getValue():
+    num1 = int(e1.get())
+    num2 = int(e2.get())
+    print num1 + num2
+
+root = Tk()
+
+v1 = StringVar()
+v2 = StringVar()
+e1 = Entry(root, textvariable = v1)
+e2 = Entry(root, textvariable = v2)
+
+b = Button(root, command = getValue, text="Test")
+e1.pack()
+e2.pack()
+b.pack()
+v1.set("Tal 1")
+v2.set("Tal 2")
+
+
+
+root.mainloop()
